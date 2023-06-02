d020d1c09f3543311a802a0550a586c90c66177d
Antoine Vandermeersch <avdmeers@gmail.com>
2023-06-02 15:21:59 +0200

    synology: no delete + cron working directory

--- brainfartlab/overlord/roles/synology/files/sync-up.sh
+++ brainfartlab/overlord/roles/synology/files/sync-up.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-#if [ -z "$SSH_AGENT_PID" ]; then
-#  eval $(ssh-agent -s)
-#fi
-#ssh-add $HOME/.ssh/id_ed25519_synology
-
 timestamp=$(date +"%Y%m%d%H%M%S")
 
 rsync -aivz \
   --exclude-from filters.txt \
-  --delete \
   ~/Documents/ brainfartlab@synology:/volume1/backup/brainfartlab > $HOME/backup/up/$timestamp.log
