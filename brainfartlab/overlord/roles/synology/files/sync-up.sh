ab0c82a53e82bf13e3c781d897290990aa7d15f8
Antoine Vandermeersch <avdmeers@gmail.com>
2023-06-02 20:38:31 +0200

    synology: templating & fixes

--- brainfartlab/overlord/roles/synology/files/sync-up.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-timestamp=$(date +"%Y%m%d%H%M%S")
-
-rsync -aivz \
-  --exclude-from filters.txt \
-  ~/Documents/ brainfartlab@synology:/volume1/backup/brainfartlab > $HOME/backup/up/$timestamp.log
