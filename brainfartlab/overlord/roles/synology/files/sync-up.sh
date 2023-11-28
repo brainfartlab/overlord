db3ccfd9ba101bff9a9ebbd2f5304fb2bcec8422
Antoine Vandermeersch <avdmeers@gmail.com>
2023-11-28 15:09:24 +0100

    limit file upload to < 50MiB

--- brainfartlab/overlord/roles/synology/files/sync-up.sh
+++ brainfartlab/overlord/roles/synology/files/sync-up.sh
@@ -6,6 +6,7 @@ RSYNC_FILTERS_PATH=${3:-/usr/local/share/brainfartlab/synology/filters.txt}
 timestamp=$(date +"%Y%m%d%H%M%S")
 
 rsync -aivz \
+  --max-size=50m \
   --exclude-from $RSYNC_FILTERS_PATH \
   -e "ssh -i $SYNOLOGY_KEY" \
   ~/Documents/ $SYNOLOGY_USER@synology:/volume1/backup/$SYNOLOGY_USER > $HOME/.backup/up-$timestamp.log
